301ede670d5c798b15324c243761d12ab0c3ef71
Nick Milazzo <nmils820@gmail.com>
2018-03-20 12:48:42 -0400

    1

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 //const mapboxgl = require('mapbox-gl')
-const mapboxgl = require('mapbox-gl/dist/mapbox-gl.js')
-const markerMakerFunc = require('./marker.js').markerMakerFunc
+const mapboxgl = require('mapbox-gl/dist/mapbox-gl.js');
+const markerMakerFunc = require('./marker.js').markerMakerFunc;
 
 mapboxgl.accessToken =
-  'pk.eyJ1Ijoic2lhbWVzZWd1eSIsImEiOiJjamV6dGJwNmwwZnprMzFvYTVqZXR6eWF3In0.A391dwHbc5ypIjMcA3SV9g'
+  'pk.eyJ1Ijoic2lhbWVzZWd1eSIsImEiOiJjamV6dGJwNmwwZnprMzFvYTVqZXR6eWF3In0.A391dwHbc5ypIjMcA3SV9g';
 
 const map = new mapboxgl.Map({
   container: 'map',
@@ -12,11 +12,11 @@ const map = new mapboxgl.Map({
   style: 'mapbox://styles/mapbox/streets-v10' // mapbox has lots of different map styles available.
 })
 
-const marker = markerMakerFunc('restaurant', -72.009151, 40.705086)
-marker.addTo(map)
+const marker = markerMakerFunc('restaurant', -74.009151, 40.705086);
+marker.addTo(map);
 
-const fullstack = markerMakerFunc('hotel', -74.009, 40.705)
-fullstack.addTo(map)
+const fullstack = markerMakerFunc('hotel', -74.009, 40.705);
+fullstack.addTo(map);
 
 // const markerDomEl = document.createElement('div')
 // markerDomEl.style.width = '32px'
--- src/marker.js
+++ src/marker.js
@@ -1,16 +1,17 @@
 const mapboxgl = require('mapbox-gl/dist/mapbox-gl.js')
 
 const markerMakerFunc = function(type, lat, long) {
-  const markerDomEl = document.createElement('div')
-  markerDomEl.style.width = '32px'
-  markerDomEl.style.height = '39px'
-  if (type === 'activity')
-    markerDomEl.style.backgroundImage = 'url(http://i.imgur.com/WbMOfMl.png)'
-  else if (type === 'hotel')
-    markerDomEl.style.backgroundImage = 'url(http://i.imgur.com/D9574Cu.png)'
-  else markerDomEl.style.backgroundImage = 'url(http://i.imgur.com/cqR6pUI.png)'
+  const markerDomEl = document.createElement('div');
+  markerDomEl.style.width = '32px';
+  markerDomEl.style.height = '39px';
+  if (type === 'activity') {
+    markerDomEl.style.backgroundImage = 'url(http://i.imgur.com/WbMOfMl.png)';
+  }
+  else if (type === 'hotel') {
+    markerDomEl.style.backgroundImage = 'url(http://i.imgur.com/D9574Cu.png)';
+  } else markerDomEl.style.backgroundImage = 'url(http://i.imgur.com/cqR6pUI.png)';
 
-  return new mapboxgl.Marker(markerDomEl).setLngLat([lat, long])
+  return new mapboxgl.Marker(markerDomEl).setLngLat([lat, long]);
 }
 
 module.exports = {
